4e8b3badb4800e59897013b7defa22c0e18007a7
Nicholas Basdeo <90708587+nbasdeo01@users.noreply.github.com>
2023-04-18 18:27:14 -0400

    Fixed indentation error in Bill_detect2.py

--- Cash_Detection/Bill_detect2.py
+++ Cash_Detection/Bill_detect2.py
@@ -38,6 +38,7 @@ def detect_cash(target_amount):
                     frame_label.config(image=photo)
                     frame_label.image = photo
             window.update_idletasks()
+            
     def capture_frames():
         global cap, running, shared_frame
         cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM),format=NV12,width=640,height=480,framerate=30/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink drop=1", cv2.CAP_GSTREAMER)
@@ -82,68 +83,68 @@ def detect_cash(target_amount):
             with frame_lock:
                 frame = shared_frame.copy()
             if process_frame:
-                    process_frame = False
-                    ret, frame = cap.read()
-                    if ret:
+                process_frame = False
+                ret, frame = cap.read()
+                if ret:
                 # Convert frame to a blob to feed into YOLOv3
-                        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+                    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
 
-                        # Feed the blob into the neural network and get the output predictions
-                        net.setInput(blob)
-                        layer_outputs = net.forward(net.getUnconnectedOutLayersNames())
+                    # Feed the blob into the neural network and get the output predictions
+                    net.setInput(blob)
+                    layer_outputs = net.forward(net.getUnconnectedOutLayersNames())
 
-                        # Initialize lists for detected cash objects and their corresponding monetary values
-                        cash_objects = []
-                        cash_values = []
-                        # Loop through each output layer and detect cash objects
-                        for output in layer_outputs:
-                            for detection in output:
-                                scores = detection[5:]
-                                class_id = np.argmax(scores)
-                                confidence = scores[class_id]
-                                if confidence > 0.5 and (classes[class_id].startswith("dollar_") or classes[class_id].startswith("coin_")):
-                                    # Extract bounding box coordinates and monetary value of cash object
-                                    box = detection[:4] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
-                                    cash_objects.append(box.astype("int"))
-                                    if classes[class_id].startswith("dollar_"):
-                                        cash_values.append(int(classes[class_id].split("_")[1]))
-                                    else:
-                                        cash_values.append(0.01 if classes[class_id] == "coin_penny" else 0.05 if classes[class_id] == "coin_nickel" else 0.1 if classes[class_id] == "coin_dime" else 0.25)
-                        indices = []
-                        if len(cash_objects) > 0:
-                            indices = cv2.dnn.NMSBoxes(cash_objects, [1.0]*len(cash_objects), 0.5, 0.5)
-                        else: 
-                            indices=[]
-                    
+                    # Initialize lists for detected cash objects and their corresponding monetary values
+                    cash_objects = []
+                    cash_values = []
+                    # Loop through each output layer and detect cash objects
+                    for output in layer_outputs:
+                        for detection in output:
+                            scores = detection[5:]
+                            class_id = np.argmax(scores)
+                            confidence = scores[class_id]
+                            if confidence > 0.5 and (classes[class_id].startswith("dollar_") or classes[class_id].startswith("coin_")):
+                                # Extract bounding box coordinates and monetary value of cash object
+                                box = detection[:4] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
+                                cash_objects.append(box.astype("int"))
+                                if classes[class_id].startswith("dollar_"):
+                                    cash_values.append(int(classes[class_id].split("_")[1]))
+                                else:
+                                    cash_values.append(0.01 if classes[class_id] == "coin_penny" else 0.05 if classes[class_id] == "coin_nickel" else 0.1 if classes[class_id] == "coin_dime" else 0.25)
+                    indices = []
+                    if len(cash_objects) > 0:
+                        indices = cv2.dnn.NMSBoxes(cash_objects, [1.0]*len(cash_objects), 0.5, 0.5)
+                    else: 
+                        indices=[]
+                
 
-                        for i in np.array(indices).flatten():
-                            current_box = cash_objects[i]
-                            matched_prev_box = False
+                    for i in np.array(indices).flatten():
+                        current_box = cash_objects[i]
+                        matched_prev_box = False
 
-                            # Compare current_box to each box in prev_cash_objects using the Intersection over Union (IoU) metric
-                            for detected_obj in detected_objects:
-                                if iou(current_box[:4], detected_obj['box'][:4]) > 0.5:
-                                    matched_prev_box = True
-                                    break
-                                        
-                    # Only add the cash value if current_box does not match any box in prev_cash_objects
-                            if not matched_prev_box:
-                                total_amount += cash_values[i]
-                                detected_objects.append({"box": current_box, "ttl": frames_to_live})
-                        detected_objects = [{"box": obj["box"], "ttl": obj["ttl"] - 1} for obj in detected_objects if obj["ttl"] > 0]
+                        # Compare current_box to each box in prev_cash_objects using the Intersection over Union (IoU) metric
+                        for detected_obj in detected_objects:
+                            if iou(current_box[:4], detected_obj['box'][:4]) > 0.5:
+                                matched_prev_box = True
+                                break
+                                    
+                # Only add the cash value if current_box does not match any box in prev_cash_objects
+                        if not matched_prev_box:
+                            total_amount += cash_values[i]
+                            detected_objects.append({"box": current_box, "ttl": frames_to_live})
+                    detected_objects = [{"box": obj["box"], "ttl": obj["ttl"] - 1} for obj in detected_objects if obj["ttl"] > 0]
 
 
-                        if len(indices)>0:
-                            cash_values = [cash_values[int(i)]for i in indices]
-                        else:
-                            cash_values=[]       
-                # Check if target amount has been reached
-                        if total_amount >= target_amount:
-                            target_reached = True
+                    if len(indices)>0:
+                        cash_values = [cash_values[int(i)]for i in indices]
+                    else:
+                        cash_values=[]       
+            # Check if target amount has been reached
+                    if total_amount >= target_amount:
+                        target_reached = True
 
-                    # Display message when target amount is reached
-                        cv2.putText(frame, "Target amount reached!", (10, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
-                        break
+                # Display message when target amount is reached
+                    cv2.putText(frame, "Target amount reached!", (10, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+                    break
         # Display the total amount and change required on the frame
             frame_lock.acquire()
             cv2.putText(frame, "Total amount: ${:.2f}".format(total_amount), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
